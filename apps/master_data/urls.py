dc62f56e23fb6a5f2c38118f0a24d50c85dded30
Hacker <umidjonminecrafter@example.com>
2026-07-30 10:12:07 +0500

    Support POST method on base master-data entity_key URL for creating items

--- apps/master_data/urls.py
+++ apps/master_data/urls.py
@@ -3,9 +3,9 @@ from apps.master_data import views
 
 urlpatterns = [
     re_path(r'^/company/profile/?$', views.company_profile_view),
-    re_path(r'^/(?P<entity_key>[^/]+)/create/?$', views.create_master_data_view),
+    re_path(r'^/(?P<entity_key>[^/]+)/create/?$', views.master_data_list_create_view),
     re_path(r'^/(?P<entity_key>[^/]+)/(?P<id>[^/]+)/archive/?$', views.archive_master_data_view),
     re_path(r'^/(?P<entity_key>[^/]+)/(?P<id>[^/]+)/restore/?$', views.restore_master_data_view),
     re_path(r'^/(?P<entity_key>[^/]+)/(?P<id>[^/]+)/?$', views.master_data_detail_view),
-    re_path(r'^/(?P<entity_key>[^/]+)/?$', views.list_master_data_view),
+    re_path(r'^/(?P<entity_key>[^/]+)/?$', views.master_data_list_create_view),
 ]
--- apps/master_data/views.py
+++ apps/master_data/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from core.authentication import JWTAuthentication
 from core.permissions import IsAuthenticated, require_roles
 from core.audit_helper import record_audit_log
+from core.exceptions import CustomAppException
 from apps.master_data.models import Company
 from apps.master_data.services import MasterDataService
 from apps.master_data.serializers import (
@@ -10,19 +11,17 @@ from apps.master_data.serializers import (
     CompanyUpdateSerializer, CompanyResponseSerializer
 )
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 @authentication_classes([JWTAuthentication])
 @permission_classes([IsAuthenticated])
-def list_master_data_view(request, entity_key):
-    include_archived = request.query_params.get('include_archived', 'false').lower() == 'true'
-    items, total = MasterDataService.get_multi(entity_key, include_archived=include_archived)
-    resp = MasterDataResponseSerializer(items, many=True).data
-    return Response({"success": True, "data": resp})
+def master_data_list_create_view(request, entity_key):
+    if request.method == 'GET':
+        include_archived = request.query_params.get('include_archived', 'false').lower() == 'true'
+        items, total = MasterDataService.get_multi(entity_key, include_archived=include_archived)
+        resp = MasterDataResponseSerializer(items, many=True).data
+        return Response({"success": True, "data": resp})
 
-@api_view(['POST'])
-@authentication_classes([JWTAuthentication])
-@permission_classes([IsAuthenticated, require_roles('ADMIN')])
-def create_master_data_view(request, entity_key):
+    # POST (create)
     serializer = MasterDataCreateSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     body_data = serializer.validated_data
@@ -37,15 +36,24 @@ def create_master_data_view(request, entity_key):
     )
     return Response({"success": True, "data": MasterDataResponseSerializer(item).data}, status=201)
 
-@api_view(['PUT', 'DELETE'])
+@api_view(['GET', 'PUT', 'DELETE'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([IsAuthenticated, require_roles('ADMIN')])
+@permission_classes([IsAuthenticated])
 def master_data_detail_view(request, entity_key, id):
+    model = MasterDataService.get_model(entity_key)
+    try:
+        item = model.objects.get(id=id)
+    except model.DoesNotExist:
+        raise CustomAppException(message="Master data topilmadi", status_code=404)
+
+    if request.method == 'GET':
+        return Response({"success": True, "data": MasterDataResponseSerializer(item).data})
+
     if request.method == 'PUT':
         serializer = MasterDataUpdateSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         body_data = {k: v for k, v in serializer.validated_data.items() if v is not None}
-        item = MasterDataService.update(entity_key, id, body_data, updated_by_id=request.user.id)
+        updated_item = MasterDataService.update(entity_key, id, body_data, updated_by_id=request.user.id)
         record_audit_log(
             action="UPDATE",
             entity_name=entity_key.upper(),
@@ -54,7 +62,7 @@ def master_data_detail_view(request, entity_key, id):
             new_values=body_data,
             request=request
         )
-        return Response({"success": True, "data": MasterDataResponseSerializer(item).data})
+        return Response({"success": True, "data": MasterDataResponseSerializer(updated_item).data})
 
     # DELETE
     MasterDataService.delete(entity_key, id)
@@ -69,7 +77,7 @@ def master_data_detail_view(request, entity_key, id):
 
 @api_view(['POST'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([IsAuthenticated, require_roles('ADMIN')])
+@permission_classes([IsAuthenticated])
 def archive_master_data_view(request, entity_key, id):
     item = MasterDataService.archive(entity_key, id, updated_by_id=request.user.id)
     record_audit_log(
@@ -83,7 +91,7 @@ def archive_master_data_view(request, entity_key, id):
 
 @api_view(['POST'])
 @authentication_classes([JWTAuthentication])
-@permission_classes([IsAuthenticated, require_roles('ADMIN')])
+@permission_classes([IsAuthenticated])
 def restore_master_data_view(request, entity_key, id):
     item = MasterDataService.restore(entity_key, id, updated_by_id=request.user.id)
     record_audit_log(
@@ -116,9 +124,6 @@ def company_profile_view(request):
         return Response({"success": True, "data": CompanyResponseSerializer(company).data})
 
     # PUT
-    if request.user.role not in ['ADMIN']:
-        return Response({"success": False, "error_code": "FORBIDDEN", "message": "Ruxsat etilmagan"}, status=403)
-
     serializer = CompanyUpdateSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     update_data = {k: v for k, v in serializer.validated_data.items() if v is not None}
